fb8cac6ef2622b6debc4bdbe4f36b94dab4c0965
Shams Al Miad <shamsalm0@gmail.com>
2024-06-03 11:40:14 +0600

    Create Customer Reviews page

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -19,23 +19,23 @@ export default function Login() {
       <div className="flex justify-center items-center w-full h-screen p-5 ">
         <div className="h-[680px] lg:w-[550px] md:w-[550px]  w-full shadow-2xl border-t rounded-2xl py-14 pb-5 lg:px-20 md:px-20  flex flex-col  ">
           <div className="mx-auto">
-            <Image className="" src={logo} width={226} height={72} alt="" />
+            <Image className="" src={logo} width={150} height={62} alt="" />
           </div>
           <div className="pt-6 mb-5 text-[1.5rem] font-semibold text-[#2F65EC] text-center mx-auto">
             <h2>PayGet Sign in</h2>
           </div>
           <form onSubmit={handleLogin}>
-            <div className="border my-6 mx-auto  px-2 py-2 lg:mx-0 lg:py-4 lg:px-4 bg-white focus-within:border-[#2F65EC] hover:border-[#2F65EC] rounded-md w-3/4 lg:w-full">
+            <div className="border my-6 mx-auto  lg:mx-0   bg-white focus-within:border-[#2F65EC] hover:border-[#2F65EC] rounded-md w-3/4 lg:w-full">
               <input
-                className=" w-full focus:border-[#2F65EC] outline-none"
+                className=" w-full px-2 py-2 lg:py-3 lg:px-3 bg-transparent rounded-md outline-none"
                 type="text"
                 name="username"
                 placeholder="UserName"
               />
             </div>
-            <div className="border my-6 mx-auto  px-2 py-2 lg:mx-0 lg:py-4 lg:px-4 bg-white focus-within:border-[#2F65EC] hover:border-[#2F65EC] rounded-md w-3/4 lg:w-full">
+            <div className="border my-6 mx-auto   lg:mx-0  bg-white focus-within:border-[#2F65EC] hover:border-[#2F65EC] rounded-md w-3/4 lg:w-full">
               <input
-                className=" w-full  bg-white  outline-none"
+                className=" w-full px-2 py-2 lg:py-3 lg:px-3 bg-transparent rounded-md outline-none"
                 type="password"
                 name="password"
                 placeholder="Password"
---
+++ app/customer-reviews/page.js
@@ -0,0 +1,199 @@
+import React from "react";
+import Reviews from "../_components/Reviews/Reviews";
+import img1 from "@/app/_assets/r1.jpg";
+import img2 from "@/app/_assets/r2.jpg";
+import img3 from "@/app/_assets/r3.jpg";
+import Image from "next/image";
+const CustomerReview = () => {
+  const reviews = [
+    {
+      id: 1,
+      image: img1,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "ALPER KAMU",
+      designation: "UI Developer",
+    },
+    {
+      id: 2,
+      image: img2,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "David Kameron",
+      designation: "Frontend Developer",
+    },
+    {
+      id: 3,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 4,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 5,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 6,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 7,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 8,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 9,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 10,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 11,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 12,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 13,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 14,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 15,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 16,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 17,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+    {
+      id: 18,
+      image: img3,
+      description:
+        "Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Lomer Smith",
+      designation: "Businessman",
+    },
+  ];
+  const minifiedReviews = reviews?.slice(0, 18);
+  return (
+    <div>
+      <section className="">
+        <div className="container mx-auto mt-20">
+          <div className=" mb-10">
+            <h2 className="text-center text-[#443f35] text-[44px] font-extrabold">
+              Customer Reviews
+            </h2>
+            <p className="text-xl text-blue-500 text-center font-normal">
+              See what people saying about us
+            </p>
+          </div>
+          <div className="border-b"></div>
+          <div className="flex flex-wrap gap-8 justify-center mt-10">
+            {minifiedReviews.map((review) => (
+              <div
+                className="lg:w-[28%] lg:mb-0 mb-6 p-4 border rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:rotate-x-15 hover:rotate-y-15"
+                key={review.id}
+              >
+                <div className="h-full text-center">
+                  <Image
+                    alt="testimonial"
+                    className="w-32 h-32 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src={review.image}
+                    width={200}
+                    height={200}
+                    priority
+                  />
+                  <p className="leading-relaxed">{review.description}</p>
+                  <span className="inline-block h-1 w-10 rounded bg-blue-600 mt-6 mb-4"></span>
+                  <h2 className="text-gray-900 font-bold title-font tracking-wider text-sm">
+                    {review.name}
+                  </h2>
+                  <p className="text-gray-500">{review.designation}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default CustomerReview;
