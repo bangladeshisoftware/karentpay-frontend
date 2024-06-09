f219273f282091425955cb760c297620cb472b93
Shams Al Miad <shamsalm0@gmail.com>
2024-06-09 15:46:27 +0600

    add production page

--- app/dashboard/components/home/Test.js
+++ app/dashboard/components/home/Test.js
@@ -1,41 +1,188 @@
-'use client';
-import React,{useState,useEffect} from 'react';
-import ApiRequest from '@/app/_lib/Api_request';
-
+"use client";
+import React, { useState, useEffect } from "react";
+import ApiRequest from "@/app/_lib/Api_request";
+import Link from "next/link";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Test = () => {
-    const [key, setKey] = useState({});
-    useEffect(() => {
-        getTestKey();
-    },[]);
+  const [key, setKey] = useState({});
+  const [copyMessage1, setCopyMessage1] = useState("");
+  const [copyMessage2, setCopyMessage2] = useState("");
+  const [showText, setShowText] = useState(false);
 
-    useEffect(()=>{
-        console.log(key[0]);
-    },[key])
+  useEffect(() => {
+    getTestKey();
+  }, []);
 
-    const getTestKey = async () => {
-        const response = await ApiRequest({
-            url: '/key',
-            method: 'get',
-        });
-        if (response.status == 200) {
-            setKey(response.data);
-        } else {
-            console.log(response);
-        }
+  useEffect(() => {
+    console.log(key[0]);
+  }, [key]);
 
-        console.log(key);
-    };
+  const getTestKey = async () => {
+    const response = await ApiRequest({
+      url: "/key",
+      method: "get",
+    });
+    if (response.status === 200) {
+      setKey(response.data);
+    } else {
+      console.log(response);
+    }
 
+    console.log(key);
+  };
 
-    return (
-        <div className=" ml-5">
-          <p className="text-xl font-bold"> {key[0]?.name} </p>  
-          <p className="text-xl font-bold">Privet Key : <span> {key[0]?.public_key} </span> </p>  
-          <p className="text-xl font-bold">Public Key : <span> {key[0]?.privet_key} </span></p>  
+  const handleCopy1 = (text) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        setCopyMessage1("Copied!");
+        setTimeout(() => setCopyMessage1(""), 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  };
 
+  const handleCopy2 = (text) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        setCopyMessage2("Copied!");
+        setTimeout(() => setCopyMessage2(""), 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+      });
+  };
+
+  const handleRevealText = () => {
+    setShowText(true);
+  };
+  const handleHideText = () => {
+    setShowText(false);
+  };
+
+  return (
+    <div className="container ml-0 mt-5">
+      <h2 className="text-3xl font-bold">Developers</h2>
+      <div className="border-b">
+        <p className="mt-2">API keys</p>
+      </div>
+      <div className="w-full border p-3 mt-3 rounded-md flex items-center justify-between">
+        <h3 className="text-xl font-semibold">API keys</h3>
+        <Link className="" href="#">
+          <span className="text-sm text-[#2F65EC] font-medium flex items-center">
+            Learn more about API Authentication{" "}
+            <IoIosArrowRoundForward fontSize={20} />
+          </span>
+        </Link>
+      </div>
+
+      <div className="mt-5 border rounded-md">
+        <div className="border-b p-4">
+          <h3 className="text-xl font-semibold">Standard keys</h3>
+          <p className="text-sm font-normal">
+            Create a key that unlocks full API access, enabling extensive
+            interaction with your account.{" "}
+            <Link href="#">
+              <span className="text-[#2F65EC]">Learn more</span>
+            </Link>
+          </p>
         </div>
-    )}
+        <div className="">
+          <table>
+            <thead>
+              <tr className="w-full text-left  mt-3 h-8 border-b ">
+                <th className="w-[500px]  text-xs font-medium">
+                  <div className="ml-4">NAME</div>
+                </th>
+                <th className="lg:w-[500px] font-medium text-xs">TOKEN</th>
+                <th className="lg:w-[400px] font-medium text-xs">LAST USED</th>
+                <th className="lg:w-[400px] font-medium text-xs">CREATED</th>
+                <th className="lg:w-[100px]"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-xs ml-4 h-20 border-b">
+                <td>
+                  <div className="ml-3 font-semibold">Publishable key</div>
+                </td>
+                <td
+                  className="break-words cursor-pointer relative "
+                  onClick={() =>
+                    handleCopy1(
+                      "pk_test_51POXHyGQoiRj0oYpQgjiiaOxOnhhmT7ZFvGQUdAOV9aRJSM4i50shZI6ICgXtw3bhXzIINJDkRh2o8ioxxEWpT8V00w4lYTsM3"
+                    )
+                  }
+                  onMouseEnter={() => setCopyMessage1("Click to copy")}
+                  onMouseLeave={() => setCopyMessage1("")}
+                >
+                  {copyMessage1 && (
+                    <div className="absolute -top-6 left-0 bg-gray-200 text-xs p-1 rounded">
+                      {copyMessage1}
+                    </div>
+                  )}
+                  pk_test_51POXHyGQoiRj0oYpQgjiiaOxOnhhmT7
+                  ZFvGQUdAOV9aRJSM4i50shZI6ICgXtw
+                  3bhXzIINJDkRh2o8ioxxEWpT8V00w4lYTsM3
+                </td>
+                <td>-</td>
+                <td>9jun</td>
+                <td>...</td>
+              </tr>
+              <tr className="text-xs mt-4">
+                <td>
+                  <div className="ml-3 font-semibold">Secret key</div>
+                </td>
+                <td className="relative">
+                  <span
+                    className={`relative cursor-pointer ${
+                      !showText ? "blur-lg" : ""
+                    }`}
+                    onClick={() =>
+                      handleCopy2(
+                        "pk_test_51POXHyGQoiRj0oYpQgjiiaOxOnhhmT7ZFvGQUdAOV9aRJSM4i50shZI6ICgXtw3bhXzIINJDkRh2o8ioxxEWpT8V00w4lYTsM3"
+                      )
+                    }
+                    onMouseEnter={() => setCopyMessage2("Click to copy")}
+                    onMouseLeave={() => setCopyMessage2("")}
+                  >
+                    {copyMessage2 && (
+                      <div className="absolute -top-6 left-0 bg-gray-200 text-xs p-1 rounded">
+                        {copyMessage2}
+                      </div>
+                    )}
+                    pk_test_51POXHyGQoiRj0oYpQgjiiaOxOnhhmT7
+                    ZFvGQUdAOV9aRJSM4i50shZI6ICgXtw
+                    3bhXzIINJDkRh2o8ioxxEWpT8V00w4lYTsM3 <br />
+                  </span>
+                  {showText ? (
+                    <button
+                      className="bg-white border rounded-md shadow-md p-1"
+                      onClick={handleHideText}
+                    >
+                      Hide Token
+                    </button>
+                  ) : (
+                    <button
+                      className="bg-white border absolute top-4 left-20 rounded-md shadow-md p-1"
+                      onClick={handleRevealText}
+                    >
+                      Reveal Token
+                    </button>
+                  )}
+                </td>
+                <td>-</td>
+                <td>9jun</td>
+                <td>...</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Test;
-
