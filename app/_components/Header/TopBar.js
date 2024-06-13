55211cc4228b77ef9b391341a65032c105268f4d
Developeralihasan404439 <developeralihasn777@gmail.com>
2024-06-13 08:42:09 +0600

    added dashboard

--- app/_components/Header/TopBar.js
+++ app/_components/Header/TopBar.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import BecomeMerchant from '@/app/_components/Header/BecomeMerchent/BecomeMerchant';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -9,6 +9,9 @@ import { toast } from 'react-toastify';
 
 
 const TopBar = () => {
+  const [dropdownDefaultButton, setDropdownDefaultButton] = useState(false);
+  const dropdown = useRef(null);
+  const trigger = useRef(null);
 
   const [authToken, setAuthToken] = useState(null);
   useEffect(() => {
@@ -38,6 +41,17 @@ const TopBar = () => {
     console.log( deleteToken);
    }
   }
+
+  useEffect(() => {
+    const clickHandler = ({ target }) => {
+      if (!dropdown.current) return;
+      if (!dropdownDefaultButton || dropdown.current.contains(target) || trigger.current.contains(target)) return;
+      setDropdownDefaultButton(false);
+    };
+    document.addEventListener('click', clickHandler);
+    return () => document.removeEventListener('click', clickHandler);
+  }, [dropdownDefaultButton]);
+
   return (
     <section className='container flex justify-between items-center my-4 hidden lg:flex wide-laptop:flex small-laptop:flex'>
       {/* large screens */}
@@ -75,6 +89,32 @@ const TopBar = () => {
             </span>
           </button>
           }
+
+
+<div className='relative'>
+            <button
+              ref={trigger}
+              onClick={() => setDropdownDefaultButton(!dropdownDefaultButton)}
+              className='relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'
+            >
+              <span className='relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded group-hover:bg-opacity-0 flex items-center gap-1'>
+                Dropdown
+              </span>
+            </button>
+            {dropdownDefaultButton &&
+              <div className=" bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute button-0 left-0 " style={{ zIndex: '1000' }} ref={dropdown}>
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                  <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-[#351476] hover:text-white dark:hover:text-white">My Admin</a>
+                  </li>
+                  <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-[#351476] hover:text-white dark:hover:text-white">Sign out</a>
+                  </li>
+                </ul>
+              </div>
+            }
+          </div>
+
 
         </div>
       </div>
