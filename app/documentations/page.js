b0ade1c03cf1dab4dd4fc35a50269e09048d19e7
Arif <111865239+Arifbd1@users.noreply.github.com>
2024-07-09 09:25:56 +0600

    add

--- app/documentations/page.js
+++ app/documentations/page.js
@@ -1,6 +1,30 @@
-import React from 'react'
+'use client';
+import React,{useState,useEffect} from 'react';
+import ApiRequest from "@/app/_lib/Api_request";
+import { GetCookies } from "@/app/_lib/cookiesSetting";
+import { toast } from "react-toastify";
 
 const Documentations = () => {
+const[data,setData]=useState([]);
+  useEffect(() => {
+    fatchData();
+  }, []);
+
+  const fatchData = async () => {
+    const token = await GetCookies({ name: "auth_token" });
+    if (token) {
+      const response = await ApiRequest({
+        url: "/get_documentation",
+        method: "get",
+      });
+      if (response.status == 200) {
+       console.log(response);
+      } else {
+        toast.error(response.message);
+      }
+    }
+  };
+
   return (
     <div>Documentations</div>
   )
