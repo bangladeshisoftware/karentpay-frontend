318a391243f607d5e3bef546c14a07c508585048
Arif <111865239+Arifbd1@users.noreply.github.com>
2024-07-11 13:31:07 +0600

    add

--- app/dashboard/components/Wtransactions.js
+++ app/dashboard/components/Wtransactions.js
@@ -301,7 +301,7 @@ function Wtransactions() {
                         </td>
                         {/* <td className="px-4 py-3 flex items-center justify-end"></td> */}
                       </tr>
-                    ))}shamimlem@yahoo.com
+                    ))}
                   </tbody>
                 </table>
               </div>
