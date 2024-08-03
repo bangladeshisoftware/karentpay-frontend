8cb23b5453d54121ea9acb96030f4637c7a546aa
Khalid Hasan <iamkhalid.hs@gmail.com>
2024-08-03 18:18:32 +0600

    update documentation get logic

--- app/documentations/page.js
+++ app/documentations/page.js
@@ -1,61 +1,42 @@
-"use client";
-import ApiRequest from "@/app/_lib/Api_request";
-import { GetCookies } from "@/app/_lib/cookiesSetting";
+import { useGetDocumentation } from "@/lib/useGetDocumentation";
 import parse from "html-react-parser";
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
 
-const Documentations = () => {
-  const options = {
-    wordwrap: 130,
-  };
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true); // State to manage loading status
-  const [error, setError] = useState(null); // State to manage error
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    try {
-      const response = await ApiRequest({
-        url: "/get_documentation",
-        method: "get",
-      });
-
-      if (response.status === 200) {
-        setData(response.data);
-      } else {
-        toast.error(response.message);
-      }
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+export default async function Documentation() {
+  const documentations = await useGetDocumentation();
 
   return (
-    <div className="flex justify-center">
-      <div className="scale-x-95 lg:scale-x-100 w-[98%] lg:w-[60%]">
-        <div className="mt-[70px] space-y-5">
-          {data?.map((item, index) => (
-            <div
-              key={item?.id}
-              className=" bg-white rounded-md px-5 py-4 lg:px-10 lg:py-5 text-justify"
-            >
-              <h2 className="text-2xl my-2 gradient-text">{item.title}</h2>
-              <p className="text-lg text-gray-700">
-                {parse(`${item.description}`)}
-              </p>
+    <>
+      {documentations?.length > 0 ? (
+        <div className="flex justify-center">
+          <div className="scale-x-95 lg:scale-x-100 w-[98%] lg:w-[60%]">
+            <div className="mt-[70px] space-y-5">
+              {documentations?.map((item) => (
+                <div
+                  key={item?.id}
+                  className=" bg-white rounded-md px-5 py-4 lg:px-10 lg:py-5 text-justify"
+                >
+                  <h2 className="text-2xl my-2 gradient-text">{item.title}</h2>
+                  <div className="text-lg text-gray-700">
+                    {parse(`${item.description}`)}
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="flex justify-center">
+          <div className="scale-x-95 lg:scale-x-100 w-[98%] lg:w-[60%]">
+            <div className="mt-[70px] space-y-5">
+              <div className=" bg-white rounded-md px-5 py-4 lg:px-10 lg:py-5 text-justify">
+                <h2 className="text-2xl my-2 gradient-text">
+                  No documentation found
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
-};
-
-export default Documentations;
+}
