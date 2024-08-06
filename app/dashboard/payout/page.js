4ecb9bcf7bdda0a9a207ec57db769a35c347c5a9
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2024-08-06 17:42:41 +0600

    fix round

--- app/dashboard/payout/page.js
+++ app/dashboard/payout/page.js
@@ -196,7 +196,7 @@ function Payout() {
                   </div>
                 </div>
               </div>
-              <div className="overflow-x-auto h-[55vh]">
+              <div className="overflow-x-auto h-[60vh]">
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
