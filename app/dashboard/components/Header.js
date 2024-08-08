83e2ebcdcb80278678ee6bb818038a7724c64425
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2024-08-08 14:03:41 +0600

    dev and test mode added

--- app/dashboard/components/Header.js
+++ app/dashboard/components/Header.js
@@ -7,14 +7,12 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineSettings } from "react-icons/md";
 import { CiCirclePlus } from "react-icons/ci";
 
-function Header({ isOn, toggleSwitch }) {
-  // make use EEffect
+function Header({ isOn, setIsOn}) {
 
-  // const [isOn, setIsOn] = useState(true);
 
-  // const toggleSwitch = () => {
-  //   setIsOn(!isOn);
-  // };
+  const toggleSwitch = () => {
+    setIsOn(!isOn);
+  };
   return (
     <div className="w-full pt-1 pb-3 mt-0  lg:py-4 lg:mt-3">
       <div className=" border shadow-lg mb-4 lg:mb-2 p-3 lg:p-3 mt-3 rounded-md text-center lg:text-left lg:hidden  ">
@@ -33,22 +31,20 @@ function Header({ isOn, toggleSwitch }) {
             onClick={() => toggleSwitch()}
           >
             <p
-              className={`${
-                isOn
-                  ? "bg-gradient-2"
-                  : "bg-[#111857]"
-              } w-24 h-10 rounded-md text-center flex items-center justify-center text-sm text-white font-bold`}
+              className={`${isOn
+                ? "bg-gradient-2"
+                : "bg-[#111857]"
+                } w-24 h-10 rounded-md text-center flex items-center justify-center text-sm text-white font-bold`}
               onClick={() => toggleSwitch}
             >
               Test mode
             </p>
 
             <p
-              className={`${
-                !isOn
-                  ? "bg-gradient-2"
-                  : "bg-[#111857]"
-              } w-24 h-10 rounded-md text-center flex items-center justify-center text-sm text-white font-bold`}
+              className={`${!isOn
+                ? "bg-gradient-2"
+                : "bg-[#111857]"
+                } w-24 h-10 rounded-md text-center flex items-center justify-center text-sm text-white font-bold`}
               onClick={() => toggleSwitch}
             >
               Dev mode
--- app/dashboard/developer/page.js
+++ app/dashboard/developer/page.js
@@ -1,11 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Test from "../components/home/Test";
 import Production from "../components/home/Production";
+import Header from "../components/Header";
 
-export default function Developer({ isTest }) {
+export default function Developer() {
+
+  const [isOn, setIsOn] = useState(true);
+
   return (
+
     <div className=" bg-white rounded-md pt-3 ml-0  px-1 lg:px-2 h-full">
-      {isTest ? <Test /> : <Production/>}
+      <Header
+        setIsOn={setIsOn}
+        isOn={isOn}
+      ></Header>
+      {isOn ? <Test /> : <Production />}
     </div>
   );
 }
